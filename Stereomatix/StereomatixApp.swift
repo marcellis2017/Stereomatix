4ec21879803d2fd4cb3d78318299fd95d4a1192c
Marc Ellis <mhe.mex@gmail.com>
2026-07-06 08:41:08 +0200

    Initial Commit

---
+++ Stereomatix/StereomatixApp.swift
@@ -0,0 +1,17 @@
+//
+//  StereomatixApp.swift
+//  Stereomatix
+//
+//  Created by Marc Ellis on 06/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct StereomatixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
